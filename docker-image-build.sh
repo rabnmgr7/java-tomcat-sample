a32432ce22ec1e75010b5c9d209a831c728f6b47
rabnmgr7 <rabnmgr7@gmail.com>
2024-01-21 12:00:15 +0000

    scripts edited

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -1,3 +1,5 @@
+echo "Logging in to Harbor Registry!"
+docker login harbor.registry.local || true
 echo "Building docker image"
 docker image build -t harbor.registry.local/java-tomcat-sample/1-21-maven-app:v1 .
 echo "Pushing image to harbor registry"
