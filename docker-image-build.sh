0b038ca31aeb62d573a2b0c3f4e27f84efbdb04c
rabnmgr7 <rabnmgr7@gmail.com>
2024-01-22 12:07:18 +0000

    Environment variable included

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -1,9 +1,7 @@
-echo "Removing Previously built image."
-docker image rm -f harbor.registry.local/java-tomcat-sample/1-21-maven-app:v1 || true
 echo "Logging in to Harbor Registry!"
 docker login harbor.registry.local || true
 echo "Building docker image"
-docker image build -t harbor.registry.local/java-tomcat-sample/1-21-maven-app:v1 .
+docker image build -t harbor.registry.local/java-tomcat-sample/1-21-maven-app:$BUILD_NUMBER .
 echo "Pushing image to harbor registry"
-docker push harbor.registry.local/java-tomcat-sample/1-21-maven-app:v1
+docker push harbor.registry.local/java-tomcat-sample/1-21-maven-app:$BUILD_NUMBER
 
