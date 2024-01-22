7b093777162bef91dc90d8c0d48864b03a05a622
rabnmgr7 <rabnmgr7@gmail.com>
2024-01-22 12:18:05 +0000

    container creation script edited

--- docker-container-create.sh
+++ docker-container-create.sh
@@ -1,6 +1,6 @@
 echo "Removing previously build container"
 docker container rm -f java-tomcat-sample-container || true
 echo "Pulling image from harbor registry!!"
-docker image pull harbor.registry.local/java-tomcat-sample/1-21-maven-app:v1
+docker image pull harbor.registry.local/java-tomcat-sample/1-21-maven-app:$BUILD_NUMBER
 echo "Running container for image:"
-docker run -itd --name java-tomcat-sample-container -p 8083:8080 harbor.registry.local/java-tomcat-sample/1-21-maven-app:v1
+docker run -itd --name java-tomcat-sample-container -p 8083:8080 harbor.registry.local/java-tomcat-sample/1-21-maven-app:$BUILD_NUMBER
