573a4e2016401fd0de191c0ead5cb59a0b713e7b
rabnmgr7 <rabnmgr7@gmail.com>
2024-01-25 10:53:19 +0545

    registry push and pull part || trued

--- docker-container-create.sh
+++ docker-container-create.sh
@@ -3,6 +3,6 @@ docker container stop java-tomcat-sample-container || true
 echo "Removing previously built container"
 docker container rm java-tomcat-sample-container || true
 echo "Pulling image from harbor registry!!"
-docker image pull harbor.registry.local/java-tomcat-sample/1-21-maven-app:$BUILD_NUMBER
+docker image pull harbor.registry.local/java-tomcat-sample/1-21-maven-app:$BUILD_NUMBER || true
 echo "Running container for image:"
 docker run -itd --name java-tomcat-sample-container -p 8083:8080 harbor.registry.local/java-tomcat-sample/1-21-maven-app:$BUILD_NUMBER
--- docker-image-build.sh
+++ docker-image-build.sh
@@ -3,5 +3,5 @@ docker login harbor.registry.local || true
 echo "Building docker image"
 docker image build -t harbor.registry.local/java-tomcat-sample/1-21-maven-app:$BUILD_NUMBER .
 echo "Pushing image to harbor registry"
-docker push harbor.registry.local/java-tomcat-sample/1-21-maven-app:$BUILD_NUMBER
+docker push harbor.registry.local/java-tomcat-sample/1-21-maven-app:$BUILD_NUMBER || true
 
